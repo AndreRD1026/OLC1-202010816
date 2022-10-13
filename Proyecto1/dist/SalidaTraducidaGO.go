f5ebef030c5637fff2608d946592cffb30fb8a14
AndreRD1026 <davilacesar125@gmail.com>
2022-10-12 22:02:27 -0600

    Cambios

---
+++ Proyecto1/dist/SalidaTraducidaGO.go
@@ -0,0 +1,180 @@
+package main
+import	"fmt"
+func main() {
+var variable1 float64  = 
+var variable1 float64  = 5
+var variable2 float64  = 
+var variable2 float64  = 50
+var v3 string  = "esta es una cadena"
+var v2 string  = "esta es una cadena"
+var v1 string  = "esta es una cadena"
+var curso1 string  = "olc"
+var curso2 string  = "olc"
+var curso3 string  = "olc"
+var pi1 float64  = 
+var pi1 float64  = 3
+var pi2 float64  = 
+var pi2 float64  = 3.1
+var pi3 float64  = 
+var pi3 float64  = 3.14
+var pi4 float64  = 
+var pi4 float64  = 3.141
+var anio1 float64  = 
+var anio1 float64  = 1
+var anio2 float64  = 
+var anio2 float64  = 9
+var anio3 float64  = 
+var anio3 float64  = 4
+var anio4 float64  = 
+var anio4 float64  = 5
+var variableAntesNeg float64  = 
+var variableAntesNeg float64  = 5.0
+var encabezado1 string  = "Universidad San Carlos de Guatemala...;"
+var encabezado2 string  = "Escuela de Ciencias y Sistemas
+Segundo semestre
+"
+var flag1 bool  = true
+var flag2 bool  = false
+var name1 byte  = 'f'
+var name2 byte  = 'e'
+var name3 byte  = 'r'
+var name6 byte  = 'n'
+var name4 byte  = 'n'
+var name5 byte  = 'a'
+var name7 byte  = 'd'
+var name8 byte  = 'o'
+var operaciones1Basica float64  = 
+var operaciones1Basica float64  = 1 + (1)
+var operaciones1Basica2 float64  = 
+var operaciones1Basica2 float64  = _operaciones1Basica_ + _operaciones1Basica_
+var operaciones1Intermedia float64  = 
+var operaciones1Intermedia float64  = 15 + (9 * 8) + 200 / 8 * 3 + 9
+var operaciones1Avanzadas1 float64  = 
+var operaciones1Avanzadas1 float64  = ((15 + 9) * 8 + 200 / 8 * 3 + 9)
+var operaciones1Avanzadas2 float64  = math.Pow(float64((1**[1] + 2)),float64([22.2 - 2.2 - 15]))
+
+var operaciones1Avanzadas2 float64  = (1**[1] + 2)**[22.2 - 2.2 - 15] + (2)
+var operaciones1Avanzadas3 float64  = math.Pow(float64(30),float64([2]))
+
+var operaciones1Avanzadas3 float64  = (30**[2]) + (2)
+var operaciones1Avanzadas4 float64  = math.Pow(float64(30),float64([10 - 8 + 9 - 4 * 2 - 1]))
+
+var operaciones1Avanzadas4 float64  = (30**[10 - 8 + 9 - 4 * 2 - 1]) + (2)
+var operaciones1Avanzadas5 float64  = math.Pow(float64(1),float64([1]))
+
+var operaciones1Avanzadas5 float64  = 30**[10 - 8 + 9 - 4 * 2 - 1] + (15 / (1**[1] + 2))
+var operaciones1Avanzadas6 float64  = math.Pow(float64(1),float64([1]))
+
+var operaciones1Avanzadas6 float64  = (5 * 8)%(1 + 5 + 6)
+var operaciones1Avanzadas7 float64  = math.Pow(float64((5 + 3)),float64([8]))
+
+var operaciones1Avanzadas7 float64  = (5 * 8)%((5 + 3)**[8])
+var operaciones1Avanzadas8 float64  = math.Pow(float64((5 + (2**[2 * 2]))),float64([8]))
+
+var operaciones1Avanzadas8 float64  = (5 * 8)%((5 + (2**[2 * 2]))**[8])
+var operacionRela1 bool  = 5 + 5>5
+var operacionRela31 bool  = _operaciones1Basica_>8
+var operacionRela32 bool  = (_operaciones1Basica_ + 6)>=8
+var operacionRela33 bool  = (_operaciones1Basica_ + 6)<=8
+var operacionRela4 bool  = _operaciones1Basica_==8
+var operacionRela31 bool  = (_operaciones1Basica_ + 6)<8
+var operacionRela5 bool  = _operaciones1Basica_==_operaciones1Basica_
+var operacionRela6 bool  = _operaciones1Basica_==_operaciones1Basica_ + 1
+var operacionRela7 bool  = _operaciones1Basica_==(_operaciones1Basica_) * (8 + 5)
+var operacionRela5 bool  = _operaciones1Basica_!=_operaciones1Basica_
+var v1 string  = "esta es la cadena numero 1"
+var v3 string  = "estas cadenas deben ser diferentes"
+var v2 string  = "estas cadenas deben ser diferentes"
+var curso3 string  = "Organizacion de lenguajes y compiladores 1"
+var curso2 string  = "Organizacion de lenguajes y compiladores 1"
+var curso1 string  = "Organizacion de lenguajes y compiladores 1"
+fmt.Println(operaciones1Basica)
+fmt.Println(operaciones1Basica2)
+fmt.Println(operaciones1Intermedia)
+fmt.Println(operaciones1Avanzadas1)
+fmt.Println(operaciones1Avanzadas2)
+fmt.Println(operaciones1Avanzadas3)
+fmt.Println(operaciones1Avanzadas4)
+fmt.Println(operaciones1Avanzadas5)
+fmt.Println(operaciones1Avanzadas6)
+fmt.Println(operaciones1Avanzadas7)
+fmt.Println(operaciones1Avanzadas8)
+fmt.Println(operacionRela1)
+fmt.Println(operacionRela31)
+fmt.Println(operacionRela32)
+fmt.Println(operacionRela33)
+fmt.Println(operacionRela4)
+fmt.Println(operacionRela31)
+fmt.Println(operacionRela5)
+fmt.Println(operacionRela6)
+fmt.Println(operacionRela7)
+fmt.Println(operacionRela5)
+fmt.Println(encabezado1)
+fmt.Println(encabezado2)
+fmt.Println("...")
+fmt.Println(anio1)
+fmt.Println(anio2)
+fmt.Println(anio3)
+fmt.Println(anio4)
+fmt.Println(".")
+fmt.Println(v3)
+fmt.Println(name1)
+fmt.Println(name2)
+fmt.Println(name3)
+fmt.Println(name4)
+fmt.Println(name5)
+fmt.Println(name6)
+fmt.Println(name7)
+fmt.Println(name8)
+fmt.Println(".")
+fmt.Println(variable1)
+fmt.Println(variable2)
+fmt.Println(v1)
+fmt.Println(v2)
+fmt.Println(v3)
+fmt.Println(curso1)
+fmt.Println(curso2)
+fmt.Println(curso3)
+fmt.Println(pi1)
+fmt.Println(pi2)
+fmt.Println(pi3)
+fmt.Println(pi4)
+fmt.Println(anio1)
+fmt.Println(anio2)
+fmt.Println(anio3)
+fmt.Println(anio4)
+fmt.Println(variableAntesNeg)
+fmt.Println(flag1)
+fmt.Println(flag2)
+fmt.Println("Al parecer no funciona la asignacion, restame puntos en el if")
+fmt.Println(variable1)
+var variable1 float64  = _variable1_ + 1
+fmt.Println("no tiene que imprimir este mensaje, error en instruccion if")
+fmt.Println("INTRUCCION IF con Else esta correcta")
+fmt.Println("no tiene que imprimir este mensaje,error en l aintruccion if")
+fmt.Println("no tiene que imprimir este mensaje,error en l aintruccion if")
+fmt.Println("no tiene que imprimir este mensaje,error en l aintruccion if")
+fmt.Println("Instruccion if con else if esta correcta")
+var varB bool  = false
+var i float64  = math.Pow(float64((5 + (2**[2 * 2]))),float64([8]))
+
+var i float64  = 0
+var acumulado float64  = math.Pow(float64((5 + (2**[2 * 2]))),float64([8]))
+
+var acumulado float64  = 0
+var acumulado float64  = _acumulado_ + _acumulado_
+fmt.Println(" Esta es la potencia Manual")
+fmt.Println(acumulado)
+var potencia float64  = math.Pow(float64(_base_),float64([_exponente_]))
+
+var potencia float64  = _base_**[_exponente_]
+fmt.Println(" Esta es la potencia Manual pero con una funcion")
+fmt.Println(potencia)
+return _potencia_
+fmt.Println("estamos entrando al metodo 1")
+fmt.Println(" Esta es la potencia Funcion")
+fmt.Println("FIN del metodo1")
+fmt.Println("El fibonacci de 5 es ")
+
+}
+
---
+++ Proyecto1/dist/SalidaTraducidaPY.py
@@ -0,0 +1,144 @@
+def main():
+	variable1 = 5
+	variable2 = 50
+	v3 = "esta es una cadena"
+	v2 = "esta es una cadena"
+	v1 = "esta es una cadena"
+	curso1 = "olc"
+	curso2 = "olc"
+	curso3 = "olc"
+	pi1 = 3
+	pi2 = 3.1
+	pi3 = 3.14
+	pi4 = 3.141
+	anio1 = 1
+	anio2 = 9
+	anio3 = 4
+	anio4 = 5
+	variableAntesNeg = 5.0
+	encabezado1 = "Universidad San Carlos de Guatemala...;"
+	encabezado2 = "Escuela de Ciencias y Sistemas
+Segundo semestre
+"
+	flag1 = True
+	flag2 = False
+	name1 = 'f'
+	name2 = 'e'
+	name3 = 'r'
+	name6 = 'n'
+	name4 = 'n'
+	name5 = 'a'
+	name7 = 'd'
+	name8 = 'o'
+	operaciones1Basica = 1 + (1)
+	operaciones1Basica2 = _operaciones1Basica_ + _operaciones1Basica_
+	operaciones1Intermedia = 15 + (9 * 8) + 200 / 8 * 3 + 9
+	operaciones1Avanzadas1 = ((15 + 9) * 8 + 200 / 8 * 3 + 9)
+	operaciones1Avanzadas2 = (1**[1] + 2)**[22.2 - 2.2 - 15] + (2)
+	operaciones1Avanzadas3 = (30**[2]) + (2)
+	operaciones1Avanzadas4 = (30**[10 - 8 + 9 - 4 * 2 - 1]) + (2)
+	operaciones1Avanzadas5 = 30**[10 - 8 + 9 - 4 * 2 - 1] + (15 / (1**[1] + 2))
+	operaciones1Avanzadas6 = (5 * 8)%(1 + 5 + 6)
+	operaciones1Avanzadas7 = (5 * 8)%((5 + 3)**[8])
+	operaciones1Avanzadas8 = (5 * 8)%((5 + (2**[2 * 2]))**[8])
+	operacionRela1 = 5 + 5>5
+	operacionRela31 = _operaciones1Basica_>8
+	operacionRela32 = (_operaciones1Basica_ + 6)>=8
+	operacionRela33 = (_operaciones1Basica_ + 6)<=8
+	operacionRela4 = _operaciones1Basica_==8
+	operacionRela31 = (_operaciones1Basica_ + 6)<8
+	operacionRela5 = _operaciones1Basica_==_operaciones1Basica_
+	operacionRela6 = _operaciones1Basica_==_operaciones1Basica_ + 1
+	operacionRela7 = _operaciones1Basica_==(_operaciones1Basica_) * (8 + 5)
+	operacionRela5 = _operaciones1Basica_!=_operaciones1Basica_
+	v1 = "esta es la cadena numero 1"
+	v3 = "estas cadenas deben ser diferentes"
+	v2 = "estas cadenas deben ser diferentes"
+	curso3 = "Organizacion de lenguajes y compiladores 1"
+	curso2 = "Organizacion de lenguajes y compiladores 1"
+	curso1 = "Organizacion de lenguajes y compiladores 1"
+	print(operaciones1Basica)
+	print(operaciones1Basica2)
+	print(operaciones1Intermedia)
+	print(operaciones1Avanzadas1)
+	print(operaciones1Avanzadas2)
+	print(operaciones1Avanzadas3)
+	print(operaciones1Avanzadas4)
+	print(operaciones1Avanzadas5)
+	print(operaciones1Avanzadas6)
+	print(operaciones1Avanzadas7)
+	print(operaciones1Avanzadas8)
+	print(operacionRela1)
+	print(operacionRela31)
+	print(operacionRela32)
+	print(operacionRela33)
+	print(operacionRela4)
+	print(operacionRela31)
+	print(operacionRela5)
+	print(operacionRela6)
+	print(operacionRela7)
+	print(operacionRela5)
+	print(encabezado1)
+	print(encabezado2)
+	print(anio1)
+	print(anio2)
+	print(anio3)
+	print(anio4)
+	print(v3)
+	print(name1)
+	print(name2)
+	print(name3)
+	print(name4)
+	print(name5)
+	print(name6)
+	print(name7)
+	print(name8)
+	print(variable1)
+	print(variable2)
+	print(v1)
+	print(v2)
+	print(v3)
+	print(curso1)
+	print(curso2)
+	print(curso3)
+	print(pi1)
+	print(pi2)
+	print(pi3)
+	print(pi4)
+	print(anio1)
+	print(anio2)
+	print(anio3)
+	print(anio4)
+	print(variableAntesNeg)
+	print(flag1)
+	print(flag2)
+	print(variable1)
+	variable1 = _variable1_ + 1
+nullif(v1==_v2_):
+
+analizadores.Nodo1@5d2659cbanalizadores.Nodo1@47b4c5f4if (v1==_v2_):
+
+analizadores.Nodo1@49e4f62c else: 
+analizadores.Nodo1@2ec0ce83
+analizadores.Nodo1@41c73291if (v1==_v2_):
+
+analizadores.Nodo1@1e318de8analizadores.Nodo1@50e30612analizadores.Nodo1@2e073d20 else: 
+analizadores.Nodo1@2e073d20
+analizadores.Nodo1@1be88a98	varB = False
+	i = 0
+	acumulado = 0
+	acumulado = _acumulado_ + _acumulado_
+for i in 0:
+analizadores.Nodo1@9a40ac0
+	print(acumulado)
+	potencia = _base_**[_exponente_]
+	print(potencia)
+null	return _potencia_
+	def metodo1():
+analizadores.Nodo1@56c6855a
+	metodo1()
+
+
+if __name__ == '__main__':
+	main()
+

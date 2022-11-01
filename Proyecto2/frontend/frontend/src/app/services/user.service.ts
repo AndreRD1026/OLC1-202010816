e4c3bb657b560365726e20e72d8321ba0dbfb802
AndreRD1026 <davilacesar125@gmail.com>
2022-11-01 16:27:12 -0600

    Front

---
+++ Proyecto2/frontend/frontend/src/app/services/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  URL = "http://localhost/8080"
+  constructor( private http:HttpClient) { }
+
+
+  getData(){
+    return this.http.get(`${this.URL}/getIncremental`);
+  }
+
+}
